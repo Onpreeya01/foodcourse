8e11a4fdd74a6aaf069f4a183d329c9308948eeb
Onpreeya <jang11072545@gmail.com>
2024-08-08 18:27:51 +0700

    Add components and arrange the layout

---
+++ foodcourse/src/layout/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Content from "../component/Content/Content";
+import Nav from "../component/Nav/Nav";
+
+function MainLayout(){
+    return (
+    <section>
+        <Nav></Nav> 
+        <Content></Content>
+    </section>
+    )
+}
+
+export default MainLayout;
