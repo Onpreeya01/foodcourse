217b4ec2e4287908cd2d00752e2f34fb5327104d
Onpreeya <jang11072545@gmail.com>
2024-08-09 02:54:37 +0700

    tags and content

--- foodcourse/src/component/Content/Content.jsx
+++ foodcourse/src/component/Content/Content.jsx
@@ -1,5 +1,68 @@
+import Tags from "../Tags/Tags";
+import "./ContentStyle.css"
+
 function Content(){
-    return <section id="content">Content</section>
+    return (
+        <section id="content">
+            <Tags></Tags>
+            <section id="the-cards">
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://via.placeholder.com/320x200"/>
+                    </div>
+                    <div className="-name">
+                        ขาหมู
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://via.placeholder.com/320x200"/>
+                    </div>
+                    <div className="-name">
+                        ข้าวมันไก่
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://via.placeholder.com/320x200"/>
+                    </div>
+                    <div className="-name">
+                        ข้าวต้ม
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://via.placeholder.com/320x200"/>
+                    </div>
+                    <div className="-name">
+                        ไข่เจียวหมูสับ
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://via.placeholder.com/320x200"/>
+                    </div>
+                    <div className="-name">
+                        แกงส้มปลากระป๋อง
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://via.placeholder.com/320x200"/>
+                    </div>
+                    <div className="-name">
+                        ยำมาม่าทะเล
+                    </div>
+                </div>
+            </section>
+        </section>
+    );
+    
 }
 
 export default Content;
--- foodcourse/src/component/Tags/Tags.jsx
+++ foodcourse/src/component/Tags/Tags.jsx
@@ -1,5 +1,22 @@
+import './TagsStyle.css'
+
 function Tags(){
-    return <div>Tags</div>
+    return (
+        <section id="tags">
+            <a href="" className="-item -active">
+                ทั้งหมด
+            </a>
+            <a href="" className="-item">
+                อาหารเช้า
+            </a>
+            <a href="" className="-item">
+                เมนูผัก
+            </a>
+            <a href="" className="-item">
+                อาหารเย็น
+            </a>
+        </section>
+    )
 }
 
 export default Tags;
