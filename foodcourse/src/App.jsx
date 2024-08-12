62544a0450acbcef64bc2f6747791f88cec5aa43
Onpreeya <jang11072545@gmail.com>
2024-08-12 20:04:32 +0700

    add path

--- foodcourse/src/App.jsx
+++ foodcourse/src/App.jsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from "./layout/MainLayout";
+import Breakfast from "./layout/Breakfast"
 
 function App() {
   return (
-    <MainLayout></MainLayout>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />} />
+        <Route path="/breakfast" element={<Breakfast />} />
+        </Routes>
+    </Router>
   )
 }
 
