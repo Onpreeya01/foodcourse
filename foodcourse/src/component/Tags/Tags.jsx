47121bd5e7f94355bc7a98591906c642d5e58ee6
Onpreeya <jang11072545@gmail.com>
2024-08-12 20:12:59 +0700

    create page and path

--- foodcourse/src/component/Tags/Tags.jsx
+++ foodcourse/src/component/Tags/Tags.jsx
@@ -1,22 +1,25 @@
+import { Link, useLocation } from 'react-router-dom';
 import './TagsStyle.css'
 
 function Tags(){
+    const location = useLocation();
+
     return (
         <section id="tags">
-            <a href="" className="-item -active">
+            <Link to="/" className={`-item ${location.pathname === '/' ? '-active' : ''}`}>
                 ทั้งหมด
-            </a>
-            <a href="" className="-item">
+            </Link>
+            <Link to="/breakfast" className={`-item ${location.pathname === '/breakfast' ? '-active' : ''}`}>
                 อาหารเช้า
-            </a>
-            <a href="" className="-item">
+            </Link>
+            <Link to="/vegetable" className={`-item ${location.pathname === '/vegetable' ? '-active' : ''}`}>
                 เมนูผัก
-            </a>
-            <a href="" className="-item">
+            </Link>
+            <Link to="/dinner" className={`-item ${location.pathname === '/dinner' ? '-active' : ''}`}>
                 อาหารเย็น
-            </a>
+            </Link>
         </section>
-    )
+    );
 }
 
 export default Tags;
