bcf5424ea4a2f4665debcd1da6f4281388dee6a5
Onpreeya <jang11072545@gmail.com>
2024-08-12 20:10:52 +0700

    add image and page

--- foodcourse/src/component/Content/Content.jsx
+++ foodcourse/src/component/Content/Content.jsx
@@ -8,7 +8,7 @@ function Content(){
             <section id="the-cards">
                 <div className="card">
                     <div className="-thumb">
-                        <img src="https://via.placeholder.com/320x200"/>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLF14wfYejK3g96LLf73ZVrY9_D3DjpMAEqg&s"/>
                     </div>
                     <div className="-name">
                         ขาหมู
@@ -17,7 +17,7 @@ function Content(){
 
                 <div className="card">
                     <div className="-thumb">
-                        <img src="https://via.placeholder.com/320x200"/>
+                        <img src="https://image.bestreview.asia/wp-content/uploads/2022/09/Chicken-rice-1.jpg"/>
                     </div>
                     <div className="-name">
                         ข้าวมันไก่
@@ -26,7 +26,7 @@ function Content(){
 
                 <div className="card">
                     <div className="-thumb">
-                        <img src="https://via.placeholder.com/320x200"/>
+                        <img src="https://www.bloggang.com/data/b/benyarat088/picture/1674731717.jpg"/>
                     </div>
                     <div className="-name">
                         ข้าวต้ม
@@ -35,7 +35,7 @@ function Content(){
 
                 <div className="card">
                     <div className="-thumb">
-                        <img src="https://via.placeholder.com/320x200"/>
+                        <img src="https://s.isanook.com/wo/0/ud/47/239973/tnw8.jpg?ip/crop/w670h402/q80/jpg"/>
                     </div>
                     <div className="-name">
                         ไข่เจียวหมูสับ
@@ -44,7 +44,7 @@ function Content(){
 
                 <div className="card">
                     <div className="-thumb">
-                        <img src="https://via.placeholder.com/320x200"/>
+                        <img src="https://img-global.cpcdn.com/recipes/ae44ff9fad017bb4/680x482cq70/%E0%B8%A3%E0%B8%9B-%E0%B8%AB%E0%B8%A5%E0%B8%81-%E0%B8%82%E0%B8%AD%E0%B8%87-%E0%B8%AA%E0%B8%95%E0%B8%A3-%E0%B9%81%E0%B8%81%E0%B8%87%E0%B8%AA%E0%B8%A1%E0%B8%A1%E0%B8%B0%E0%B8%A5%E0%B8%B0%E0%B8%81%E0%B8%AD%E0%B8%9B%E0%B8%A5%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%9B%E0%B8%AD%E0%B8%87-sour-curry-with-sardines.jpg"/>
                     </div>
                     <div className="-name">
                         แกงส้มปลากระป๋อง
@@ -53,7 +53,7 @@ function Content(){
 
                 <div className="card">
                     <div className="-thumb">
-                        <img src="https://via.placeholder.com/320x200"/>
+                        <img src="https://s359.kapook.com/pagebuilder/dd8fa42d-01a2-48c1-b146-a0770bf7d499.jpg"/>
                     </div>
                     <div className="-name">
                         ยำมาม่าทะเล
---
+++ foodcourse/src/layout/Breakfast.jsx
@@ -0,0 +1,24 @@
+import Nav from "../component/Nav/Nav";
+import Tags from "../component/Tags/Tags";
+import './Breakfast.css';
+
+function Breakfast() {
+    return (
+        <section id="break-fast">
+            <Nav />
+            <Tags/>
+            <section id="the-cards">
+                <div className="card">
+                    <div className="-thumb">
+                        <img src="https://www.bloggang.com/data/b/benyarat088/picture/1674731717.jpg"/>
+                    </div>
+                    <div className="-name">
+                        ข้าวต้ม
+                    </div>
+                </div>
+            </section>
+        </section>
+    );
+}
+
+export default Breakfast;
